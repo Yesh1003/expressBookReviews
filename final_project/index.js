162b4a5a98631573d8f9121b362687208434c7a9
Yeshwanth B V <yeshwanthb@vrize.com>
2023-05-29 18:49:20 +0530

    Commit changes

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,20 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+  const token = req.headers.authorization || req.query.token || req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    // Verify and decode the token
+    const decoded = jwt.verify(token, 'your_secret_key');
+    req.user = decoded.user; // Store the user information in the request object
+    next();
+  } catch (error) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
 });
  
 const PORT =5000;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,95 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const axios = require('axios');
+const baseURL = 'http: localhost/*'; // Replace with your API base URL
+
+
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+  const { username, password } = req.body;
+
+  // Check if the username is already taken
+  const existingUser = users.find(user => user.username === username);
+  if (existingUser) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  // Create a new user object
+  const newUser = { username, password };
+
+  // Add the new user to the users array
+  users.push(newUser);
+
+  return res.status(200).json({ message: "User registered successfully" });
 });
 
-// Get the book list available in the shop
+// Get the book list available in the shop using promise callbacks
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  axios.get(`${baseURL}/books`)
+    .then(response => {
+      const bookList = response.data.filter(book => book.available);
+      return res.status(200).json(bookList);
+    })
+    .catch(error => {
+      console.error(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN using promise callbacks
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  
+    const isbn = req.params.isbn;
+  
+    axios.get(`${baseURL}/books/${isbn}`)
+      .then(response => {
+        const bookDetails = response.data;
+        return res.status(200).json(bookDetails);
+      })
+      .catch(error => {
+        console.error(error);
+        return res.status(500).json({ message: "Internal Server Error" });
+      });
+  
  });
   
-// Get book details based on author
+// Get book details based on authorusing promise callbacks
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  axios.get(`${baseURL}/books?author=${author}`)
+    .then(response => {
+      const bookList = response.data;
+      return res.status(200).json(bookList);
+    })
+    .catch(error => {
+      console.error(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
-// Get all books based on title
+// Get all books based on title using promise callbacks
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  axios.get(`${baseURL}/books?title=${title}`)
+    .then(response => {
+      const bookList = response.data;
+      return res.status(200).json(bookList);
+    })
+    .catch(error => {
+      console.error(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookList = books.filter(book => book.available); // Filter available books
+  return res.status(200).json(bookList);
 });
 
 module.exports.general = public_users;
